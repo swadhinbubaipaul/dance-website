9a2a0419b285037de582b3bb64267d77ac464af6
swadhinbubaipaul <swadhinpaul159@gmail.com>
2023-07-08 13:41:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dance Website
+
+A dance academy website using Pure Pug + NodeJs
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// Express SPECIFIC STUFF
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// ENDPOINTS
+app.get("/", (req, res) => {
+  const params = {};
+  res.status(200).render("home.pug", params);
+});
+
+app.get("/contact", (req, res) => {
+  const params = {};
+  res.status(200).render("contact.pug", params);
+});
+
+// START THE SERVER
+app.listen(port, () => {
+  console.log(`The application started successfully on port ${port}`);
+});
